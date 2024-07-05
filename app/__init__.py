d1f7e35481a6e461ea46e256cc774035a0d537f8
Tom Watts <tomwattspanda@live.com>
2024-07-05 16:49:52 +0100

    Created a generic placeholder template 'base.html' to be inherited after further styling

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ import secrets
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)  # Generate a random 32-character hexadecimal string as secret key
 
-from .views import main
+from .main import main
 app.register_blueprint(main)
 
+from .auth import auth
+app.register_blueprint(auth)
 
