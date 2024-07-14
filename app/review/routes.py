ae89e00ea05fe837818267752dcde4bbb411bfa1
Tom Watts <tomwattspanda@live.com>
2024-07-14 13:21:21 +0100

    Improved the layout of the card review screen. Cards are now displayed in a rectangle to give the impression of authentic flashcards. Cards can be flipped by clicking "show answer" or pressing spacebar for enhanced usability. Flipping of cards implemented using basic Javascript.

--- app/review/routes.py
+++ app/review/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for, flash, abort, request, session
+from flask import Blueprint, render_template, redirect, abort, url_for, flash, request, session
 from flask_login import current_user, login_required
-from app import db
 from app.decks.models import Deck, Card
-from . import review_blueprint
+
+review_blueprint = Blueprint('review', __name__, template_folder='templates')
 
 @review_blueprint.route('/start_review/<int:deck_id>', methods=['GET'])
 @login_required
@@ -15,7 +15,7 @@ def start_review(deck_id):
     session['current_card_index'] = 0
     return redirect(url_for('review.show_card'))
 
-@review_blueprint.route('/show_card')
+@review_blueprint.route('/show_card', methods=['GET'])
 @login_required
 def show_card():
     card_ids = session.get('card_ids')
@@ -30,25 +30,17 @@ def show_card():
 @review_blueprint.route('/process_review/<int:card_id>', methods=['POST'])
 @login_required
 def process_review(card_id):
-    correct = request.form.get('correct') == 'true'
+    correct = request.form.get('correct') == 'True'
     card = Card.query.get_or_404(card_id)
-
-    # For now, just process whether the answer was correct or not
-    # Logic for updating review status, etc., can be added here
-
+    # Here you can add logic to update review status based on 'correct'
     session['current_card_index'] += 1
     return redirect(url_for('review.show_card'))
+
 
 @review_blueprint.route('/review_summary')
 @login_required
 def review_summary():
     return render_template('review_summary.html')
 
-
-@review_blueprint.route('/review')
-@login_required
-def review():
-    user_decks = Deck.query.filter_by(user_id=current_user.user_id).all()
-    return render_template('review.html', decks=user_decks)
 
 
