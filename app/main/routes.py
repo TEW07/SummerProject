f1ebe7f2c61f46d0b4188b9d70b452d0d4be55cc
Tom Watts <tomwattspanda@live.com>
2024-07-18 15:21:37 +0100

    Upgraded the dashboard so that a user will be notified of any reviews that are due and provides the details and links user to decks with cards due for review

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, flash
 from . import main_blueprint
-
-from flask_login import current_user
+from app.decks.models import Deck, Card
+from datetime import datetime
+from flask_login import current_user, login_required
 
 
 @main_blueprint.route('/')
@@ -9,10 +10,35 @@ def index():
     return render_template('home.html')
 
 
-@main_blueprint.route('/dashboard', methods=['GET', 'POST'])
+@main_blueprint.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html', username=current_user.username,
-                           log_count=current_user.get_login_count(current_user.user_id),
-                           log_latest=current_user.get_latest_login(current_user.user_id))
+    user_decks = Deck.query.filter_by(user_id=current_user.user_id).all()
+    due_reviews = []
+
+    for deck in user_decks:
+        due_count = Card.query.filter(
+            Card.deck_id == deck.deck_id,
+            Card.next_review_date <= datetime.utcnow()
+        ).count()
+        if due_count > 0:
+            due_reviews.append({
+                'deck_id': deck.deck_id,
+                'name': deck.name,
+                'due_count': due_count
+            })
+            flash(f'{due_count} cards due for review in {deck.name}', 'warning')
+
+    user_info = {
+        'username': current_user.username,
+        'login_count': current_user.get_login_count(current_user.user_id),
+        'last_login_at': current_user.get_latest_login(current_user.user_id)
+    }
+
+
+    return render_template('dashboard.html', due_reviews=due_reviews, user_info=user_info)
+
+
+
 
 
