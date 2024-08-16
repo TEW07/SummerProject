3c780927023503877f8acabe23c8e9d1e8a92118
Tom Watts <tomwattspanda@live.com>
2024-08-16 17:55:06 +0100

    Increase User.password_hash field size after receiving error

--- app/auth/models.py
+++ app/auth/models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     points = db.Column(db.Integer, default=0)  # Add points column
     achievements = db.relationship('UserAchievement', back_populates='user', cascade='all, delete-orphan')
 
